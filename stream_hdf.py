b0d7406b7a29b6522b9b4db37995062c22a77024
angulartist <belabesmohammed@gmail.com>
2020-03-31 18:44:03 +0000

    better hdf generator ++ incremental learning explroation

--- stream_hdf.py
+++ stream_hdf.py
@@ -1,24 +1,35 @@
 import h5py as h5
 
-def dictify(x):
-    return dict(zip(['feat'] * x.shape[0], x))
 
-def streamer():
-    index = 0
-    
-    while True:
-        with h5.File('train.h5', 'r') as file:     
-            start, end = index, index + 1
-            X_train = file['images'][start : end]
-            y_train = file['labels'][start : end]
+def build_h5_stream(src, *, X_key, y_key):
+    with h5.File(src, 'r') as source:
+        columns = [f'feat_{feature_idx}'
+                   for feature_idx
+                   in range(source[X_key].shape[1])]
 
-        index += 1
+    def streamer():
+        cursor = 0
 
-        yield X_train, y_train
-        
-        
-gen = streamer()
+        while True:
+            with h5.File(src, 'r') as source: 
+                try:
+                    X_train = source[X_key][cursor]
+                    y_train = source[y_key][cursor]
+                except (ValueError, IndexError):
+                    raise StopIteration('No more observations.')
 
-a, b = next(gen)
+            cursor += 1
 
-print(a.shape)
+            yield dict(zip(columns, X_train)), y_train
+            
+    return streamer()
+
+
+stream = build_h5_stream(
+    'features_train.h5',
+    X_key='images',
+    y_key='labels',
+)
+
+for a, b in stream:
+    print(b)
